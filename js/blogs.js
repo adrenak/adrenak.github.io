a50bd259853e4fa0f0e12f91c380f6abc00cf947
adrenak <vatsal@firexitsoftware.com>
2017-03-14 15:16:59 +0530

    - Added in game advertising blog (#4) - Removed a potentially offensive joke because feminazis

--- js/blogs.js
+++ js/blogs.js
@@ -20,5 +20,11 @@ var blogPosts = [
 		"blog/shadow-proxy/page.html",
 		"Shadows may be killing your game's performance. Here's a way how to fix it.",
 		"5 February 2016"
+	],
+	[
+		"In Game Advertising",
+		"blog/IGA/page.html",
+		"There's virtual real estate in your mobile games. Monetize it!",
+		"14 March 2016"
 	]
 ];
